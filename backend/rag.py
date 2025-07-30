54aa15ab57a04edc6dccf2039891267fae9a20e9
Samuel Ojayhagbega <ojayhtheog@gmail.com>
2025-07-30 15:51:19 +0100

    Second commit using Deepseek-r1

--- backend/rag.py
+++ backend/rag.py
@@ -1,16 +1,15 @@
-from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_ollama.llms import (OllamaLLM)
 from langchain_core.prompts import ChatPromptTemplate
-from vector import retriever
+from vectorr import retriever
 
 
-model =  OllamaLLM(model="mistral")
+model =  OllamaLLM(model="deepseek-r1:1.5b")
 
 template = """
-You are an expert in proferring solution to certain domestic problems
 
 Here are some relevant documentations: {answer_bank}
-Here is the problem to be solved: {question}
+
+Here is the question to answer: {question}
 """
 
 prompt_template = ChatPromptTemplate.from_template(template)
@@ -19,7 +18,6 @@ chain = prompt_template | model
 while True:
     print("\n\n------------------")
     question = input("Ask your question(q to quit): ")
-
     print("\n\n")
     if question == "q":
         break
